f3a8c66e9f6ebfbb0e96eff3f8c1b325fa01c515
nikunohi <sseanyagi@gmail.com>
2026-09-18 04:03:58 -0700

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -33,7 +33,7 @@ function splash() {
     textAlign(CENTER);
     textSize(30);
     noStroke();
-    video = createVideo(['./assets/webp/final-intro.webm']);
+    video = createVideo(['./assets/webp/Final-Intro.webm']);
     video.volume(0);
     video.hide();
     video.loop();
